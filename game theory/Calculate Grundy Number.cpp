86a68770140a1c3030cd4cebbb35e2473bec1a04
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2021-02-03 22:26:12 +0530

    Update Calculate Grundy Number.cpp

--- game theory/Calculate Grundy Number.cpp
+++ game theory/Calculate Grundy Number.cpp
@@ -1,5 +1,6 @@
 /*Calculate the Grundy Number for the given 'n' in the game.
-The game starts with a number- ‘n’ and the player to move divides the number- ‘n’ with 2, 3 or 6 and then takes the floor. If the integer becomes 0, it is removed. The last player to move wins. Which player wins the game?
+The game starts with a number- ‘n’ and the player to move divides the number- ‘n’ with 2, 3 or 6 and then takes the floor. If the integer becomes 0, it is removed. 
+The last player to move wins. Which player wins the game?
 Input Format
 An Integer 'n'
 Output Format
@@ -34,7 +35,7 @@ int grundy(int n)
     if(n <= 0)
         return 0;
     
-    return mex(grundy(n/6), grundy(n/2), grundy(n/3));
+    return mex(grundy(n/2), grundy(n/3), grundy(n/6));
 }
 int main()
 {
