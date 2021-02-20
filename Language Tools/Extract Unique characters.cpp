d668c46e53ad1dff37c7b22843c45918609b019d
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2021-02-20 11:52:18 +0530

    Create Extract Unique characters.cpp

---
+++ Language Tools/Extract Unique characters.cpp
@@ -0,0 +1,48 @@
+/*
+Given a string S, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same, as in the input string.
+Input format:
+The first and only line of input contains a string, that denotes the value of S.
+Output format :
+The first and only line of output contains the updated string, as described in the task.
+Constraints :
+0 <= Length of S <= 10^8
+Time Limit: 1 sec
+Sample Input 1 :
+ababacd
+Sample Output 1 :
+abcd
+Sample Input 2 :
+abcde
+Sample Output 2 :
+abcde
+*/
+
+#include<bits/stdc++.h>
+char* uniqueChar(char *str){
+    int n = strlen(str);
+    int i = 0;
+    map<char,int>m;
+    for(int j = 0; j < n; j++)
+    {
+        if(m[str[j]] == '\0')
+        {  m[str[j]]++;
+            str[i] = str[j];
+            i++;
+        }
+    }
+    int n1 = m.size();
+    str[n1] = '\0';
+    return str;
+   
+}
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+    string str;
+    cin >> str;
+    cout << uniqueChar(str);
+}
